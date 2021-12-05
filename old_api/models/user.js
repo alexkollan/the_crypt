8cafb92d6aa2477a4433373a8a21fcdcec6bb648
Alexander Collin Kollan <finapocalypse@gmail.com>
2021-12-05 16:30:13 +0200

    Initial commit

---
+++ old_api/models/user.js
@@ -0,0 +1,37 @@
+"use strict";
+// Import mongoose
+const mongoose = require("mongoose");
+
+// Declare schema and assign Schema class
+const Schema = mongoose.Schema;
+
+// Create Schema Instance and add schema propertise
+const user = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  userKey: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: Array,
+    required: true,
+  },
+  registrationDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create and export model
+module.exports = mongoose.model("userModel", user, "_users");
